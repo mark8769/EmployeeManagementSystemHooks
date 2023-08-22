1960e6443267dbc6292591374b5ff03b4ba19678
Mark Ortega <mortegaponce27@gmail.com>
2023-08-21 20:30:35 -0500

    spring../exception/: Added ResourceNotFoundException class.

--- springboot-backend/src/main/java/net/javaguides/springboot/exception/ResourceNotFoundException.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/exception/ResourceNotFoundException.java
@@ -1,2 +1,13 @@
-package net.javaguides.springboot.exception;public class ResourceNotFoundException {
+package net.javaguides.springboot.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+// Can also use this annotation in REST API.
+// But want to avoid repetition in controller (using ResponseStatus annotation)
+@ResponseStatus(value= HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
 }
