eebabb7220bb2ff31f63cea065daf1ff5bac05ab
Mark Ortega <mortegaponce27@gmail.com>
2023-08-21 20:32:27 -0500

    spring../repository/: Added Employee repository that extends JPA to get access to database crud functionalitys.

--- springboot-backend/src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
@@ -1,2 +1,9 @@
-package net.javaguides.springboot.repository;public class EmployeeRepository {
+package net.javaguides.springboot.repository;
+
+import net.javaguides.springboot.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// Specify the entity, and the key datatype
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    // All crud database methods to interact with database.
 }
