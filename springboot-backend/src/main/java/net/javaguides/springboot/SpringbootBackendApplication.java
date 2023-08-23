12799e6d3c07bdd007528f94d3c91bd229bc7f56
Mark Ortega <mortegaponce27@gmail.com>
2023-08-22 22:12:37 -0500

    srping.../SpringBootBackEndApplication.java: Extending command line runner in main program to create dummy data on project spin up. Autowiring service employee class as a result.

--- springboot-backend/src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -1,13 +1,40 @@
 package net.javaguides.springboot;
 
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Implementing CommandLineRunner Interface provides run method.
+// https://www.javaguides.net/2020/02/spring-boot-commandlinerunner-example.html#:~:text=CommandLineRunner%20is%20an%20interface%20used,can%20be%20ordered%20with%20%40Order.
 @SpringBootApplication
-public class SpringbootBackendApplication {
+public class SpringbootBackendApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
 	}
 
+	// Make sure to autowire the database layer..
+	// Use "constructor based" dependency injection.
+	private final EmployeeRepository employeeRepository;
+	@Autowired
+	public SpringbootBackendApplication(EmployeeRepository employeeRepository){
+		this.employeeRepository = employeeRepository;
+	}
+	@Override
+	public void run(String... args) throws Exception{
+		Employee employee = new Employee();
+		employee.setFirstName("Mark");
+		employee.setLastName("Ortega-Ponce");
+		employee.setEmailAddress("ortegam@augsburg.edu");
+		employeeRepository.save(employee);
+
+		Employee employee1 = new Employee();
+		employee.setFirstName("John");
+		employee.setLastName("Ortega");
+		employee.setEmailAddress("john@augsburg.edu");
+		employeeRepository.save(employee1);
+	}
 }
---
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -0,0 +1,2 @@
+package net.javaguides.springboot.controller;public class EmployeeController {
+}
