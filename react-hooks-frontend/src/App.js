448fbdb3754866592e991aaa555ba5ce60ab69b2
Mark Ortega <mortegaponce27@gmail.com>
2023-08-29 22:24:56 -0500

    react-frontend/src/app.js: Added routing capabilities. Using react-router-dom library. BrowserRouter -> Routes ->  Defined Routes to render some component.

--- react-hooks-frontend/src/App.js
+++ react-hooks-frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
@@ -8,9 +9,16 @@ import FooterComponent from './components/FooterComponent';
 function App() {
   return (
     <div>
-      <HeaderComponent/>
-      <ListEmployeeComponent/>
-      <FooterComponent/>
+      <Router>
+        <HeaderComponent/>
+        <div className="container">
+          <Routes> {/* Render component based on path. */}
+            <Route path="/" element={<ListEmployeeComponent/>}></Route>
+            <Route path="/employees" element={<ListEmployeeComponent/>}></Route>
+          </Routes>
+        </div>
+        <FooterComponent/>
+      </Router>
     </div>
   );
 }
