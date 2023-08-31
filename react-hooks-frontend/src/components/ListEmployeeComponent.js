00c59f4f7bc903246adcc21ad297c05b21d0b0ce
Mark Ortega <mortegaponce27@gmail.com>
2023-08-30 22:59:52 -0500

    react-frontend/src/components: Using Link component from 'react-router-dom' to add a button to add employee. Save time of creating button with function that redirects user...

--- react-hooks-frontend/src/components/ListEmployeeComponent.js
+++ react-hooks-frontend/src/components/ListEmployeeComponent.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import EmployeeService from '../services/EmployeeService'
+// Uses hyperlink internall, use this instead of a button with anchor tag inside.
+import {Link} from 'react-router-dom'
 
 const ListEmployeeComponent = () => {
 
@@ -23,6 +25,7 @@ const ListEmployeeComponent = () => {
             <h2 className="text-center">
                 List Employees
             </h2>
+            <Link to="/add-employee" className="btn btn-primary mb-2">Add Employee</Link>
             <table className="table table-border table-striped">
                 <thead>
                     <th>Employee ID</th>
