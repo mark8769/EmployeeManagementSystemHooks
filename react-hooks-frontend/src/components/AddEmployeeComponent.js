5a3c802945c12011cc582657e9e9076a2eb7ddc7
Mark Ortega <mortegaponce27@gmail.com>
2023-08-31 23:25:08 -0500

    react-frontend/src/components/AddEmployeeComponent: Defining use state for first/last name, and email variables. Adding form to add employee. Defined save employee arrow function.

--- react-hooks-frontend/src/components/AddEmployeeComponent.js
+++ react-hooks-frontend/src/components/AddEmployeeComponent.js
@@ -1,11 +1,78 @@
-import React from 'react'
+// React import is a default export.
+// Typically a library will import the default export with same name.
+// Default exports are not enclosed in braces.
+// Named exports are modules you can import form libraries
+// For example in python I could do: from datetime import time, datetime
+// Datetime is the library, my named exports would be time and datetime.
+// This makes it easier to see what functions I am using, and which ones aren't being used.
+import React, {useState} from 'react'
 
 const AddEmployeeComponent = () => {
-  return (
-    <div>
-        <h1>Add Employee</h1>
-    </div>
-  )
+
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [emailAddress, setEmailAddress] = useState("");
+
+    const saveEmployee = (e) => {
+        // Don't refresh page on form submission.
+        e.preventDefault();
+        // This looks weird at first glance...
+        // But firstName = someName
+        // lastName = someLastName ...
+        // When consoled log. The variable name is the key: its value is the value....
+        // Cool that this idea of "unpacking" comes up so much
+        const employee = {firstName, lastName, emailAddress};
+        console.log(employee);
+    }
+
+    return (
+        <div>
+            <br></br>
+            <br></br>
+            <div className="container">
+                <div className="row">
+                    <div className="card col-md-6 offset-md-3 offset-md-3">
+                        <h2 className="text-center">Add Employee</h2>
+                        <div className="card-body">
+                            <form>
+                                <div className="form-group mb-2">
+                                    <label className="form-label">First Name</label>
+                                    <input 
+                                        type="text" 
+                                        placeholder="Enter First Name"
+                                        name="firstName"
+                                        className="form-control"
+                                        value={firstName}
+                                        onChange={(e) => setFirstName(e.target.value)}>
+                                    </input>
+                                    <label className="form-label">First Name</label>
+                                    <input 
+                                        type="text" 
+                                        placeholder="Enter Last Name"
+                                        name="lastName"
+                                        className="form-control"
+                                        value={lastName}
+                                        onChange={(e) => setLastName(e.target.value)}>
+                                    </input>
+                                    <label className="form-label">Email Address</label>
+                                    <input 
+                                        type="text" 
+                                        placeholder="Enter Email Address"
+                                        name="Email Address"
+                                        className="form-control"
+                                        value={emailAddress}
+                                        onChange={(e) => setEmailAddress(e.target.value)}>
+                                    </input>
+                                    <br></br>
+                                    <button className="btn btn-success" onClick={(e) => saveEmployee(e)}>Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default AddEmployeeComponent
