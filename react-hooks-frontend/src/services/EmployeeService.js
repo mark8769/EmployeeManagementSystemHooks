44ba3d8b456d0380c2f1af1839adcf6bc10b955c
Mark Ortega <mortegaponce27@gmail.com>
2023-08-31 23:46:00 -0500

    react-frontend/src/services: Added save employee function to EmployeeServiceComponent.

--- react-hooks-frontend/src/services/EmployeeService.js
+++ react-hooks-frontend/src/services/EmployeeService.js
@@ -3,10 +3,15 @@ import axios from "axios";
 const BASE_REST_API_URL = "http://localhost:8080/api/v1/employees";
 
 class EmployeeService{
-
+    // GET
     getAllEmployees(){
         return axios.get(BASE_REST_API_URL);
     }
+    // POST
+    saveEmployee(employee){
+        return axios.post(BASE_REST_API_URL, employee);
+    }
 }
-
+// Export instance of Service 
+// Does this follow singleton principle?
 export default new EmployeeService();
