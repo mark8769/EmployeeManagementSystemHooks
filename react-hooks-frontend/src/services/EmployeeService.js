e75db7a32d92686bdc9bf50af3090d039252eccd
Mark Ortega <mortegaponce27@gmail.com>
2023-09-02 18:35:02 -0500

    react-frontend/src/services: Added methods to update and delete employees.

--- react-hooks-frontend/src/services/EmployeeService.js
+++ react-hooks-frontend/src/services/EmployeeService.js
@@ -11,6 +11,15 @@ class EmployeeService{
     saveEmployee(employee){
         return axios.post(BASE_REST_API_URL, employee);
     }
+    getEmployeeById(employeeId){
+        return axios.get(BASE_REST_API_URL + "/" + employeeId);
+    }
+    updateEmployee(employeeId, employee){
+        return axios.put(BASE_REST_API_URL + `/updateEmployee/${employeeId}`, employee);
+    }
+    deleteEmployee(employeeId){
+        return axios.delete(BASE_REST_API_URL + `/deleteEmployee/${employeeId}`);
+    }
 }
 // Export instance of Service 
 // Does this follow singleton principle?
